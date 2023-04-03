5f4fb6dbc67357052241dc979e375b3284bf4fbc
Igor Righi <cruigor@gmail.com>
2023-04-03 19:14:24 -0300

    exercicios obj for in

--- 01-Fundamentos/Secao03-Intro-JavaScript/Dia04-Objetos/script.js
+++ 01-Fundamentos/Secao03-Intro-JavaScript/Dia04-Objetos/script.js
@@ -36,3 +36,62 @@ for (let keys in car) {
 }
 
 // EXERCICIO 4
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log('Bem-vinda, ' + info.personagem)
+let recorrente = {
+  recorrente: 'Sim'
+}
+Object.assign(info, recorrente)
+//console.log(info['recorrente'])
+for (keys in info) {
+ // console.log(keys)
+}
+for (values in info) {
+ // console.log(info[values])
+}
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+for (keys in info)  {
+  if (
+    keys === 'recorrente' && info[keys] === 'Sim' && info2[keys] === 'Sim'
+  )
+  {
+    console.log('Ambos recorrente');
+  }
+  else {
+  console.log(info2[keys] + ' e ' + info[keys]);
+  }
+}
+
+// EXERCICIO 5
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'");
+
+leitor.livrosFavoritos.push (
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+)
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
