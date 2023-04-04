6c14ed0a7341cdaf73bba1339690db48336966a4
Igor Righi <cruigor@gmail.com>
2023-04-03 22:09:40 -0300

    exercicio 7

--- 01-Fundamentos/Secao03-Intro-JavaScript/Dia04-Objetos/script.js
+++ 01-Fundamentos/Secao03-Intro-JavaScript/Dia04-Objetos/script.js
@@ -95,3 +95,90 @@ leitor.livrosFavoritos.push (
   }
 )
 console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
+
+// EXERCICIO 6
+let order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+function customerInfo(order) {
+  // Adicione abaixo as informações necessárias.
+  return ('Olá, ' + order.order.delivery.deliveryPerson + ', entrega para: ' + order.name + ', Telefone: ' + order.phoneNumber + ', R. ' + order.address.street + ', N: ' + order.address.number + ', AP: ' + order.address.apartment)
+}
+
+console.log(customerInfo(order));
+
+function orderModifier(order) {
+  // Adicione abaixo as informações necessárias.
+  return ('Olá, ' + order.name + ', o valor de seu pedido de ' + Object.keys(order.order.pizza)[0] + ', ' + Object.keys(order.order.pizza)[1] + ' e ' + order.order.drinks.coke.type + ' é R$' + order.payment.total + ',00.')
+}
+
+console.log(orderModifier(order));
+
+// EXERCICIO 7
+
+let lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+let lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+let lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function addTurno(turma, turno, diaNoite) {
+  turma[turno] = diaNoite; 
+}
+addTurno(lesson2, 'turno', 'noite');
+
+function allKeys (turma) {
+  return Object.keys(turma);
+}
+
+function tamanhoObj (obj) {
+  return Object.keys(obj).length;
+}
+console.log(tamanhoObj(lesson1));
