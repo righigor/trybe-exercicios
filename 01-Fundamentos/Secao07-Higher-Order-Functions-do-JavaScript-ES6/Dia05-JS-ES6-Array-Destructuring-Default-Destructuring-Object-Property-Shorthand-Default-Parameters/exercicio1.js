0293b8b3895a1f6df4fe6b6a316ee37e0c9b45e4
Igor Righi <cruigor@gmail.com>
2023-05-11 14:06:56 -0300

    exercicios pos aula ao vivo

---
+++ 01-Fundamentos/Secao07-Higher-Order-Functions-do-JavaScript-ES6/Dia05-JS-ES6-Array-Destructuring-Default-Destructuring-Object-Property-Shorthand-Default-Parameters/exercicio1.js
@@ -0,0 +1,5 @@
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
+const swap = ([num1, num2, num3]) => [num3, num2, num1];
+console.log(swap(myList));
---
+++ 01-Fundamentos/Secao07-Higher-Order-Functions-do-JavaScript-ES6/Dia05-JS-ES6-Array-Destructuring-Default-Destructuring-Object-Property-Shorthand-Default-Parameters/exercicio2.js
@@ -0,0 +1,13 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = ([carro, marca, ano]) => {
+  return {
+    carro,
+    marca,
+    ano,
+  }
+}
+console.log(toObject(palio));
