9e9a8ac4d26db796b632895c32bf62ef4611845a
Igor Righi <cruigor@gmail.com>
2023-05-22 15:40:30 -0300

    exercicio pre aula ao vivo

---
+++ 02-Front-End/Secao01-Intro-FrontEnd-e-JavaScript-Assincrono/Dia02-JavaScript-Assincrono-Promises-e-Fetch/pre-aula-ao-vivo.js
@@ -0,0 +1,45 @@
+import './style.css';
+
+import {
+  fillUsersSelect,
+  fillPosts,
+  fillFeaturedPostComments,
+  clearPageData,
+  fillErrorMessage,
+} from './utils/updateUI';
+
+const usersSelect = document.querySelector('#users-select');
+
+const USERS_API = 'https://dummyjson.com/users';
+// faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
+fetch(USERS_API)
+  .then((response) => response.json())
+  .then((data) => {
+    const { users } = data;
+    fillUsersSelect(users);
+  });
+
+usersSelect.addEventListener('change', () => {
+  clearPageData();
+  const POSTS_API = `https://dummyjson.com/posts/user/${usersSelect.value}`
+  fetch(POSTS_API)
+    .then((response) => response.json())
+    .then((data) => {
+      const { posts } = data;
+      fillPosts(posts);
+
+      const [featuredPost] = posts;
+      const COMMENTS_API = `https://dummyjson.com/posts/${featuredPost.id}/comments`;
+
+      return fetch(COMMENTS_API);
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      const { comments } = data;
+      fillFeaturedPostComments(comments);
+    })
+    .catch((err) => {
+      fillErrorMessage('Erro ao receber informações');
+      console.log(err.message);
+    });
+});
