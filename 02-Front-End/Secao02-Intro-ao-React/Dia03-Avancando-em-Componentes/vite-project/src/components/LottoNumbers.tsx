76634ae928c59a8c69f659b1e0a32062cf6b2feb
Igor Righi <cruigor@gmail.com>
2023-05-30 15:43:07 -0300

    finalizando exercicios pre aula ao vivo

---
+++ 02-Front-End/Secao02-Intro-ao-React/Dia03-Avancando-em-Componentes/vite-project/src/components/LottoNumbers.tsx
@@ -0,0 +1,21 @@
+
+function LottoNumbers ({ numbers }: { numbers: number[] }) {
+  return (
+    <>
+      <h3>Os números sorteados são:</h3>
+      <ul>
+        {numbers.length === 6 ? (
+          numbers.map((number, index) => (
+            <li key = {index} className="lotto">
+              { number }
+            </li>
+          ))
+        ) : (
+          <h3>Erro: a lista não contém 6 números</h3>
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default LottoNumbers
