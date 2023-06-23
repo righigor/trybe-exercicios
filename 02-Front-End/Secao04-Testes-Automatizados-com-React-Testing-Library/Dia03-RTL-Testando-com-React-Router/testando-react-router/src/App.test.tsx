aad026791cec4f67a108e9acfbad2e023c34ead9
Igor Righi <cruigor@gmail.com>
2023-06-23 15:50:02 -0300

    criando tests

---
+++ 02-Front-End/Secao04-Testes-Automatizados-com-React-Testing-Library/Dia03-RTL-Testando-com-React-Router/testando-react-router/src/App.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from '@testing-library/user-event'
+import { renderWithRouter } from "./utils/renderWithRouter";
+
+import App from "./App";
+
+it('Renderiza texto da página incial', () => {
+  render(<App />, { wrapper: BrowserRouter });
+  expect(screen.getByText(/Você está na página inicial/i)).toBeInTheDocument();
+});
+
+test('Renderiza texto da página Sobre', async () => {
+  render(<App />, { wrapper: BrowserRouter });
+  const aboutLink = screen.getByRole('link', { name: /Sobre/i});
+  await userEvent.click(aboutLink);
+  expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
+});
+
+test('Renderiza página inicial', () => {
+  renderWithRouter(<App />);
+  expect(screen.getByText(/Você está na página inicial/)).toBeInTheDocument();
+});
+
+test('Navega até a página Sobre e verifica o texto', async () => {
+  const { user } = renderWithRouter(<App />);
+  expect(screen.getByText(/Você está na página inicial/)).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', { name: /Sobre/ });
+  await user.click(aboutLink);
+  expect(screen.getByText(/Você está na página Sobre/)).toBeInTheDocument();
+});
+
+test('Navega da página Sobre para página inicial', async () => {
+  const { user } = renderWithRouter(<App />, { route: '/about' });
+  expect(screen.getByText(/Você está na página Sobre/)).toBeInTheDocument();
+
+  const incialLink = screen.getByRole('link', { name: /Início/ });
+  await user.click(incialLink);
+  expect(screen.getByText(/Você está na página inicial/)).toBeInTheDocument();
+});
---
+++ 02-Front-End/Secao04-Testes-Automatizados-com-React-Testing-Library/Dia03-RTL-Testando-com-React-Router/testando-react-router/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <h1>Você está na página inicial</h1>
+  );
+}
+
+export default Home;
