b545d727b0a123afe67869d8eb6d662a7dc3300d
Igor Righi <cruigor@gmail.com>
2024-02-23 15:01:50 -0300

    exercicios

---
+++ 04-Ciencia-da-Computacao/Secao01-Intro-Python/Dia03-Poo-em-Python/pet.py
@@ -0,0 +1,18 @@
+class Pet():
+  def __init__(self, nome, especie, idade, humano):
+    self.nome = nome
+    self.especie = especie
+    self.idade = idade
+    self.humano = humano
+
+  def __str__(self) -> str:
+    return f"""
+    - Especie: {self.especie}
+    - Nome: {self.nome}
+    - Idade: {self.idade}
+    - Dono: {self.humano}
+    """
+
+thor = Pet('thor', 'gato', 5, 'felps')
+
+print(thor)
